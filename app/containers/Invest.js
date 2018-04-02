1c7b473fb567b9cfb6e3e9439493fd92163bcde4
dongyanghong <dongyanghongfe@gmail.com>
2018-04-02 17:38:12 +0800

    投资图标，切换账户

--- app/containers/Invest.js
+++ app/containers/Invest.js
@@ -14,7 +14,7 @@ class Account extends Component {
     tabBarIcon: ({ focused, tintColor }) => (
       <Image
         style={[styles.icon, { tintColor: focused ? tintColor : 'gray' }]}
-        source={require('../images/person.png')}
+        source={require('../images/investment.png')}
       />
     ),
   }

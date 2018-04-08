e448324a8e583bce63f454358f7c65b5c17c4743
dongyanghong <dongyanghongfe@gmail.com>
2018-04-08 16:49:39 +0800

    主页内容添加

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Image } from 'react-native'
+import { StyleSheet, View, Image, Text } from 'react-native'
 import Swiper from 'react-native-swiper'
 import { connect } from 'react-redux'
 
-import { Button } from '../components'
+// import { Button } from '../components'
 
-import { NavigationActions } from '../utils'
+// import { NavigationActions } from '../utils'
+import pxToDp from '../utils/pxToDp'
 
 @connect()
 class Home extends Component {
@@ -21,18 +22,15 @@ class Home extends Component {
     ),
   }
 
-  gotoDetail = () => {
-    this.props.dispatch(NavigationActions.navigate({ routeName: 'Detail' }))
-  }
+  // gotoDetail = () => {
+  //   this.props.dispatch(NavigationActions.navigate({ routeName: 'Detail' }))
+  // }
+  // <Button text="Goto Detail" onPress={this.gotoDetail} />
 
   render() {
     return (
       <View style={styles.container}>
-        <Swiper
-          style={styles.wrapper}
-          paginationStyle={{ bottom: 250 }}
-          autoplay
-        >
+        <Swiper paginationStyle={{ bottom: 100 }} autoplay>
           <View style={styles.slide1}>
             <Image source={require('../images/banner.png')} />
           </View>
@@ -43,7 +41,20 @@ class Home extends Component {
             <Image source={require('../images/banner.png')} />
           </View>
         </Swiper>
-        <Button text="Goto Detail" onPress={this.gotoDetail} />
+        <View style={styles.notice}>
+          <Image source={require('../images/volume.png')} />
+          <Text>企惠宝零钱包7日年化率提高20%!</Text>
+        </View>
+        <View>
+          <Text>
+            定期理财
+            <Text style={{ color: 'rgb(54,177,255)' }}>收益稳健</Text>
+          </Text>
+        </View>
+        <View style={styles.financial}>
+          <Text style={{ color: 'rgb(255,106,110)' }}>8%-15%</Text>
+          <Image source={require('../images/financing.png')} />
+        </View>
       </View>
     )
   }
@@ -56,10 +67,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   icon: {
-    width: 32,
-    height: 32,
+    width: pxToDp(32),
+    height: pxToDp(32),
   },
-  wrapper: {},
   slide1: {
     flex: 1,
     justifyContent: 'center',
@@ -74,6 +84,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  notice: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  financial: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
   },
 })
 

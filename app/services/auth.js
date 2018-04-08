dc26ff3efcfa3d9c9c5c13bb6bfd166799a6188e
dongyanghong <dongyanghongfe@gmail.com>
2018-04-08 09:36:44 +0800

    格式

--- app/services/auth.js
+++ app/services/auth.js
@@ -8,27 +8,16 @@ const BASE = 'http://localhost:9080/api/'
 export const login = async payload => {
   const data = await request(`${BASE}login`, {
     method: 'POST',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
     body: payload,
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-      'header-timestamp': parseInt(+new Date() / 1000),
-      'header-sign': 'D95588561747B20F191ED4A7B78302A2',
-      // 'header-token': 'deecfde049494a33affb9a11c9764aa4'
-    },
   })
   return data
 }
 export const vCode = async payload => {
   const data = await request(`${BASE}vefirycode/vefiry`, {
     method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-      'header-timestamp': parseInt(+new Date() / 1000),
-      'header-sign': 'D95588561747B20F191ED4A7B78302A2',
-    },
-    body: {
-      mobile: payload,
-    },
+    'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+    body: payload,
   })
   return data
 }
@@ -38,11 +27,6 @@ export const checkPwd = async payload => {
     method: 'POST',
     body: {
       username: payload,
-    },
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-      'header-timestamp': parseInt(+new Date() / 1000),
-      'header-sign': 'D95588561747B20F191ED4A7B78302A2',
     },
   })
   return data

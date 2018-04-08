e7fb95511457ecaf96eda89307d925b0d659441c
dongyanghong <dongyanghongfe@gmail.com>
2018-04-08 13:54:40 +0800

    auth还原

--- app/services/auth.js
+++ app/services/auth.js
@@ -2,22 +2,33 @@ import request from '../utils/request'
 // import { delay } from '../utils'
 
 // const BASE = 'https://easy-mock.com/mock/5ab7a51a89962b05a31a31cb/api/'
-const BASE = 'http://localhost:9080/api/'
-// const BASE = 'https://shunquan.corpfortune.com/api/'
+// const BASE = 'http://localhost:9080/api/'
+const BASE = 'https://shunquan.corpfortune.com/api/'
 
 export const login = async payload => {
   const data = await request(`${BASE}login`, {
     method: 'POST',
-    'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
     body: payload,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+      'header-timestamp': parseInt(+new Date() / 1000),
+      'header-sign': 'D95588561747B20F191ED4A7B78302A2',
+      // 'header-token': 'deecfde049494a33affb9a11c9764aa4'
+    },
   })
   return data
 }
 export const vCode = async payload => {
   const data = await request(`${BASE}vefirycode/vefiry`, {
     method: 'POST',
-    'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
-    body: payload,
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+      'header-timestamp': parseInt(+new Date() / 1000),
+      'header-sign': 'D95588561747B20F191ED4A7B78302A2',
+    },
+    body: {
+      mobile: payload,
+    },
   })
   return data
 }
@@ -27,6 +38,11 @@ export const checkPwd = async payload => {
     method: 'POST',
     body: {
       username: payload,
+    },
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+      'header-timestamp': parseInt(+new Date() / 1000),
+      'header-sign': 'D95588561747B20F191ED4A7B78302A2',
     },
   })
   return data

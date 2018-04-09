6cb3b0c072f7e73afe52b3382f212dc7be5cd1d4
dongyanghong <dongyanghongfe@gmail.com>
2018-04-09 16:48:48 +0800

    账户页面修改

--- app/containers/Account.js
+++ app/containers/Account.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 import { StyleSheet, View, Image, Text } from 'react-native'
 import { connect } from 'react-redux'
 
-import { Button } from '../components'
-
 import { NavigationActions } from '../utils'
 
 import pxToDp from '../utils/pxToDp'
@@ -22,12 +20,6 @@ class Account extends Component {
     ),
   }
 
-  gotoAbout = () => {
-    this.props.dispatch(NavigationActions.navigate({ routeName: 'About' }))
-  }
-  gotoSetting = () => {
-    this.props.dispatch(NavigationActions.navigate({ routeName: 'Setting' }))
-  }
   gotoMyInvest = () => {
     this.props.dispatch(NavigationActions.navigate({ routeName: 'MyInvest' }))
   }
@@ -39,20 +31,92 @@ class Account extends Component {
   gotoWithdrawal = () => {
     this.props.dispatch(NavigationActions.navigate({ routeName: 'Withdrawal' }))
   }
+  gotoSetting = () => {
+    this.props.dispatch(NavigationActions.navigate({ routeName: 'Setting' }))
+  }
+  gotoAbout = () => {
+    this.props.dispatch(NavigationActions.navigate({ routeName: 'About' }))
+  }
 
   render() {
-    // const { login } = this.props
     return (
       <View style={styles.container}>
-        <Text>134****3232</Text>
-        <Text>账户金额</Text>
-        <Text>13232.66</Text>
-        <Text>在途资金（元）23.22</Text>
-        <Button text="我的投资" onPress={this.gotoMyInvest} />
-        <Button text="资金明细" onPress={this.gotoInvestDetail} />
-        <Button text="提现" onPress={this.gotoWithdrawal} />
-        <Button text="设置" onPress={this.gotoSetting} />
-        <Button text="关于" onPress={this.gotoAbout} />
+        <Image source={require('../images/account-bg.png')} />
+        <View style={styles.wrapper}>
+          <View style={styles.wrapDeatil}>
+            <Image
+              source={require('../images/my-invest.png')}
+              style={styles.imgStyle}
+            />
+            <Text onPress={this.gotoMyInvest} style={styles.sunstance}>
+              我的投资
+            </Text>
+          </View>
+          <Image
+            source={require('../images/enter.png')}
+            style={{ width: pxToDp(24), height: pxToDp(24) }}
+          />
+        </View>
+        <View style={styles.wrapper}>
+          <View style={styles.wrapDeatil}>
+            <Image
+              source={require('../images/invest-detail.png')}
+              style={styles.imgStyle}
+            />
+            <Text onPress={this.gotoInvestDetail} style={styles.sunstance}>
+              资金明细
+            </Text>
+          </View>
+          <Image
+            source={require('../images/enter.png')}
+            style={{ width: pxToDp(24), height: pxToDp(24) }}
+          />
+        </View>
+        <View style={styles.wrapper}>
+          <View style={styles.wrapDeatil}>
+            <Image
+              source={require('../images/cash.png')}
+              style={styles.imgStyle}
+            />
+            <Text onPress={this.gotoWithdrawal} style={styles.sunstance}>
+              提现
+            </Text>
+          </View>
+          <Image
+            source={require('../images/enter.png')}
+            style={{ width: pxToDp(24), height: pxToDp(24) }}
+          />
+        </View>
+        <View style={styles.wrapper}>
+          <View style={styles.wrapDeatil}>
+            <Image
+              source={require('../images/setting.png')}
+              style={styles.imgStyle}
+            />
+            <Text onPress={this.gotoSetting} style={styles.sunstance}>
+              设置
+            </Text>
+          </View>
+          <Image
+            source={require('../images/enter.png')}
+            style={{ width: pxToDp(24), height: pxToDp(24) }}
+          />
+        </View>
+        <View style={styles.wrapper}>
+          <View style={styles.wrapDeatil}>
+            <Image
+              source={require('../images/about.png')}
+              style={styles.imgStyle}
+            />
+            <Text onPress={this.gotoAbout} style={styles.sunstance}>
+              关于
+            </Text>
+          </View>
+          <Image
+            source={require('../images/enter.png')}
+            style={{ width: pxToDp(24), height: pxToDp(24) }}
+          />
+        </View>
       </View>
     )
   }
@@ -61,12 +125,31 @@ class Account extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   icon: {
     width: pxToDp(48),
     height: pxToDp(48),
+  },
+  wrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginLeft: pxToDp(42),
+    marginTop: pxToDp(23),
+    marginBottom: pxToDp(23),
+  },
+  wrapDeatil: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  imgStyle: {
+    width: pxToDp(42),
+    height: pxToDp(42),
+    marginRight: pxToDp(26),
+  },
+  sunstance: {
+    fontSize: pxToDp(34),
+    color: 'rgb(51,51,51)',
   },
 })
 

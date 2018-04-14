50abcd0d29cc7f4745695f2cedbeeccea70d568d
fe-breeze <dongyanghongfe@gmail.com>
2018-04-14 19:14:01 +0800

    about

--- app/containers/About.js
+++ app/containers/About.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Image, Text } from 'react-native'
+import { StyleSheet, View, Image, Text, Linking } from 'react-native'
 import { connect } from 'react-redux'
 
 import { NavigationActions } from '../utils'
@@ -10,10 +10,19 @@ import pxToDp from '../utils/pxToDp'
 class Detail extends Component {
   static navigationOptions = {
     title: '关于',
+    headerStyle: {
+      backgroundColor: '#36b1ff',
+      borderBottomWidth: 0,
+    },
+    headerTintColor: '#fff',
   }
 
   gotoDetail = () => {
     this.props.dispatch(NavigationActions.navigate({ routeName: 'Detail' }))
+  }
+
+  callMe = () => {
+    Linking.openURL('tel:400-888-888')
   }
 
   render() {
@@ -28,28 +37,32 @@ class Detail extends Component {
         >
           <Image source={require('../images/logo.png')} />
         </View>
-        <View style={styles.fontWrap}>
-          <View style={[styles.packet, { marginLeft: pxToDp(20) }]}>
-            <Text style={styles.fontLeft}>版本号</Text>
+        <View style={styles.infoWrap}>
+          <View style={styles.fontWrap}>
+            <View style={[styles.packet, { marginLeft: pxToDp(20) }]}>
+              <Text style={styles.fontLeft}>版本号</Text>
+            </View>
+            <View style={[styles.packet, { marginRight: pxToDp(20) }]}>
+              <Text style={styles.fontRight}>1.1.0</Text>
+            </View>
           </View>
-          <View style={[styles.packet, { marginRight: pxToDp(20) }]}>
-            <Text style={styles.fontRight}>1.1.0</Text>
+          <View style={styles.fontWrap}>
+            <View style={[styles.packet, { marginLeft: pxToDp(20) }]}>
+              <Text style={styles.fontLeft}>官方网站</Text>
+            </View>
+            <View style={[styles.packet, { marginRight: pxToDp(20) }]}>
+              <Text style={styles.fontRight}> www.corpfortune.com</Text>
+            </View>
           </View>
-        </View>
-        <View style={styles.fontWrap}>
-          <View style={[styles.packet, { marginLeft: pxToDp(20) }]}>
-            <Text style={styles.fontLeft}>官方网站</Text>
-          </View>
-          <View style={[styles.packet, { marginRight: pxToDp(20) }]}>
-            <Text style={styles.fontRight}> www.corpfortune.com</Text>
-          </View>
-        </View>
-        <View style={styles.fontWrap}>
-          <View style={[styles.packet, { marginLeft: pxToDp(20) }]}>
-            <Text style={styles.fontLeft}>客服电话</Text>
-          </View>
-          <View style={[styles.packet, { marginRight: pxToDp(20) }]}>
-            <Text style={styles.num}>400-888-888</Text>
+          <View style={[styles.fontWrap, styles.fontWrapBottom]}>
+            <View style={[styles.packet, { marginLeft: pxToDp(20) }]}>
+              <Text style={styles.fontLeft}>客服电话</Text>
+            </View>
+            <View style={[styles.packet, { marginRight: pxToDp(20) }]}>
+              <Text style={styles.num} onPress={this.callMe}>
+                400-888-888
+              </Text>
+            </View>
           </View>
         </View>
       </View>
@@ -62,10 +75,20 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  infoWrap: {
+    paddingLeft: pxToDp(26),
+  },
   fontWrap: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    height: pxToDp(80),
+    alignItems: 'center',
+    borderTopWidth: pxToDp(1),
+    borderTopColor: '#dcdcdc',
+    height: pxToDp(88),
+  },
+  fontWrapBottom: {
+    borderBottomWidth: pxToDp(1),
+    borderBottomColor: '#dcdcdc',
   },
   packet: {
     flexDirection: 'row',
